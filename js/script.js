d0da7782b3e6f768b154ddceffd43e3849344366
Allfeeto <10509090S@mail.ru>
2023-10-22 09:49:57 +0300

    прр

--- js/script.js
+++ js/script.js
@@ -24,6 +24,8 @@ if ("vibrate" in navigator) {
 
 
 
+
+
 buyButtons.forEach((buyButton, index) => {
     buyButton.addEventListener('click', () => {
         buyButton.style.display = 'none'; // Скрываем кнопку "Купить"
